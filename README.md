263f93a54fe61fd06063ca788fb4f02aa25b3afb
GeorgeBroughton <george.daniel.broughton@gmail.com>
2024-06-17 20:33:40 +0100

    re-organized thingd

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Underground Energy Distribution
-This mod is a fork of:
+![image](docs\images\example.png)
+
+### This mod is a fork of:
  - [SLP - Underground Poles](https://mods.factorio.com/mod/slp-underground-poles) by [SLywnow](https://mods.factorio.com/user/SLywnow)
  - [SLP - Underground Poles Fix](https://mods.factorio.com/mod/slp-underground-poles-fix) by [EGOR555565](https://mods.factorio.com/user/EGOR555565)
 
-### Why fork it?
-I generally dislike the reach of the base-game electric poles and this is an attempt to improve quality of life in the game by adjusting both the recipes, graphics, and reach of the electric poles etc of the mod.
+### Why?
+I generally dislike the reach of the base-game electric poles and this is an attempt to improve quality of life in the game by adjusting both the recipes, graphics, and reach of the "electric poles" etc of the mod.
 
 ### Current State of Development
-This is currently WIP, and will have bugs that the others don't. I don't recommend installing this for now.
+This is almost ready to go. It just needs uploading to the official repository.
 
-It has been modified so it is incompatible with the originals, this is because of how differently the poles are going to work, and so it's probably best making them not cross-compatible because installing this will break your factory otherwise. You should be able to safely install this alongside the SLP mod though.
+It has been modified so it is incompatible with the originals, this is because of how differently the poles work, and so it's probably best making them not cross-compatible because installing this will break your factory. You should be able to safely install this alongside the SLP mod though.
+
+Nomenclature has been changed because it doesn't make sense to call them poles other than to be easily recognizable by the players, and i have a conniption every time i see it because they're technically called `pylons` anyway. Maybe it's just my autism showing I don't know.
+
+If you would like to know more, see the [changelog](Documentation/changelog.md)  
+I'm also writing a [graphics workflow](Documentation/Graphics%20Workflow.md) for making objects that look like they've been in a world that was lived in. A lot of modders seem to know how to make 3D looking objects, but they don't fit with the game's aesthetic.
+
+## To Do
+ - [ ] Make the icons easier to differentiate
+ - [ ] Adjust the resolution of all textures so they are consistent in screen space
+   - 1 pixel in one texture should be the same width in-game as in the other textures for consistencies sake.
--- mod-files/prototypes/small_db.lua
+++ mod-files/prototypes/small_db.lua
@@ -1,17 +1,25 @@
 local small_db=table.deepcopy(data.raw["electric-pole"]["medium-electric-pole"])
 local integration_patch_render_layer 	= "decorative"
 
-small_db.name 							= "ued-small-db"
-small_db.icon 							= "__underground-energy-distribution__/graphics/md-icon.png"
-small_db.icon_mipmaps 					= 1
+small_db.name	= "ued-small-db"
+small_db.icon	= "__underground-energy-distribution__/graphics/md-icon.png"
+
+small_db.icon_size					= 1
+small_db.drawing_box 				= {{-0.5,-0.5},{0.5,0.5}}
+small_db.selection_box				= {{-0.5,-0.5},{0.5,0.5}}
+small_db.map_generator_bounding_box	= {{-0.5,-0.5},{0.5,0.5}}
+small_db.tile_height				= 1
+small_db.tile_width					= 1
+
 small_db.minable 						= {mining_time = 0.5,result = "ued-small-db"}
-small_db.drawing_box 					= {{-0.5,-0.5},{0.5,0.5}}
 small_db.draw_copper_wires				= false
 small_db.check_collision_with_entities 	= true
 small_db.supply_area_distance 			= 5.5
 small_db.maximum_wire_distance 			= 9
 small_db.draw_circuit_wires				= settings.startup["ued-settings-show-circuit"].value
-small_db.integration_patch = {
+
+small_db.icon_mipmaps		= 1
+small_db.integration_patch	= {
 	layers = {{
 		filename 		= "__underground-energy-distribution__/graphics/md-pole.png",
 		priority 		= "low",
@@ -37,6 +45,7 @@ small_db.pictures = {
 		direction_count = 1,
 	}}
 }
+
 small_db.connection_points = {{
 	shadow = {
 		copper 	= util.by_pixel_hr(0, 0),
@@ -49,30 +58,32 @@ small_db.connection_points = {{
 		green 	= util.by_pixel_hr(0, 0)
 	}
 }}
+
 small_db.collision_mask					= {"doodad-layer"}
 --small_db.collision_mask				= {"water-tile","colliding-with-tiles-only"}
 --small_db.collision_box				= {{0,0},{0,0}}
 small_db.integration_patch_render_layer = integration_patch_render_layer
+small_db.fast_replaceable_group 		= "medium-electric-pole"
 
 data:extend({{
-	type			= "item",
-	name			= "ued-small-db",
-	icon			= "__underground-energy-distribution__/graphics/md-icon.png",
-	icon_size		= 64,
-	subgroup		= "energy-pipe-distribution",
-	order			= "a[energy]-b[medium-electric-pole]-b[ued-small-db]",
-	place_result	= "ued-small-db",
-	stack_size		= 50
-},{
-	type			= "recipe",
-	name			= "ued-small-db",
-	enabled			= "false",
-	ingredients		= {
-		{"medium-electric-pole", 1},
-		{"copper-plate", 10},
-		{"steel-plate", 5}
+		type			= "item",
+		name			= "ued-small-db",
+		icon			= "__underground-energy-distribution__/graphics/md-icon.png",
+		icon_size		= 64,
+		subgroup		= "energy-pipe-distribution",
+		order			= "a[energy]-b[medium-electric-pole]-b[ued-small-db]",
+		place_result	= "ued-small-db",
+		stack_size		= 50
+	},{
+		type			= "recipe",
+		name			= "ued-small-db",
+		enabled			= "false",
+		ingredients		= {
+			{"medium-electric-pole", 1},
+			{"copper-plate", 10},
+			{"steel-plate", 5}
+		},
+		result			= "ued-small-db"
 	},
-	result			= "ued-small-db"
-},
-small_db
+	small_db
 })
